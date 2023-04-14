6e33a004565a74224f54ce23625ff266bfdfb9e8
nitesh <nitesh.barot31@gmail.com>
2023-04-14 11:30:34 +0530

    commit 13/04

--- src/main/java/pages/Constant.java
+++ src/main/java/pages/Constant.java
@@ -10,5 +10,8 @@ public class Constant {
         } else if (ENVIRONMENT.equals("UTS-UK2Stage")) {
             url="http://52.32.106.250:8889/";
         }
+        else if (ENVIRONMENT.equals("UTS-UK2production")) {
+            url="https://fitlocal2.britishpetinsurance.co.uk:94/";
+        }
     }
 }
